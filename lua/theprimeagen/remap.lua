f17e6d15c3d617ecb5e5420074d29f6f20e22996
Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>
2025-04-03 09:27:45 -0400

    OCD

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -28,6 +28,7 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
 -- For windows, yank to clip
 -- vim.opt.clipboard = "unnamedplus"
 -- if vim.fn.has('wsl') == 1 then
