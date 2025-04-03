4eec85f8a4cb8265be187b9bcedcbbec1de7ef81
Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>
2025-04-03 09:20:09 -0400

    remove copilot and add WSL Clipboard functions

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -27,9 +27,17 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+-- vim.opt. clipboard = "unnamedplus"
+--     if vim.fn.has('wsl') == 1 then
+--        vim.api.nvim_create_autocmd('TextYankPost', {
+--        group = vim.api.nvim_create_augroup('Yank', { clear = true }),
+--        callbac
+-- 
+--     end
 -- Delete to void register
+
 vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 
 -- This is going to get me cancelled
