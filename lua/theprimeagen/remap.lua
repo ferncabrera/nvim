e2d96afbaedb368cf8d748bf334131e317aa9b9f
Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>
2025-04-03 09:26:56 -0400

    WSL

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -26,18 +26,19 @@ end
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+-- vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+-- vim.keymap.set("n", "<leader>Y", [["+Y]])
+
 -- For windows, yank to clip
--- vim.opt.clipboard = "unnamedplus"
--- if vim.fn.has('wsl') == 1 then
---     vim.api.nvim_create_autocmd('TextYankPost', {
---         group = vim.api.nvim_create_augroup('Yank', { clear = true }),
---         callback = function()
---             vim.fn.system('clip.exe', vim.fn.getreg('"'))
---         end
---     })
--- end
+vim.opt.clipboard = "unnamedplus"
+ if vim.fn.has('wsl') == 1 then
+     vim.api.nvim_create_autocmd('TextYankPost', {
+         group = vim.api.nvim_create_augroup('Yank', { clear = true }),
+         callback = function()
+             vim.fn.system('clip.exe', vim.fn.getreg('"'))
+         end
+     })
+ end
 
 -- Delete to void register
 vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
