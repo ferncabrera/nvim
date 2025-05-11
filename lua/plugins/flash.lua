3e8a14025ae5a2274705543d63b6756535867be9
Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>
2025-05-11 16:49:30 -0400

    flash colors
    Signed-off-by: Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  vscode = true,
+  ---@type Flash.Config
+  opts = {
+    label = {
+      rainbow = {
+        enabled = true,
+        shade = 5,
+      },
+    },
+  },
+}
