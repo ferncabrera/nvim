ea7036907e5be756e60fded0b91c50767ac7af80
Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>
2025-05-11 18:35:11 -0400

    updates

---
+++ lua/plugins/picker.lua
@@ -0,0 +1,37 @@
+-- Show all files for explorer, but ignore specific folders for all other file searches
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        files = {
+          hidden = true,
+          ignored = true,
+          exclude = {
+            "node_modules",
+            "dist",
+            ".git",
+            ".yarn",
+            ".venv",
+            "__pycache__",
+          },
+        },
+        explorer = {
+          ignored = true,
+          hidden = true,
+          exclude = {},
+        },
+      },
+      hidden = true,
+      ignored = true,
+      exclude = {
+        "node_modules",
+        "dist",
+        ".git",
+        ".yarn",
+        ".venv",
+        "__pycache__",
+      },
+    },
+  },
+}
