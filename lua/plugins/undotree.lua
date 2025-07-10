5422ff6453c5b4e79801b8431d69d595978259ad
Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>
2025-07-10 10:37:28 -0400

    Updates

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -3,5 +3,10 @@ return {
 
   config = function()
     vim.keymap.set("n", "<leader>tu", vim.cmd.UndotreeToggle, { desc = "Better undotree!!!" })
+    -- configure settings via Vim globals
+    vim.g.undotree_WindowLayout = 2 -- your layout config
+    -- vim.g.undotree_SplitWidth = 40 -- your layout config
+    vim.g.undotree_DiffpanelHeight = 15 -- example
+    -- add any additional opts here
   end,
 }
