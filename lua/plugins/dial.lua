32517681c878299ca702ae8cd056ac733840508e
Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>
2025-07-14 10:10:54 -0400

    updates

--- lua/plugins/dial.lua
+++ lua/plugins/dial.lua
@@ -3,7 +3,7 @@ return {
   "monaqa/dial.nvim",
     -- stylua: ignore
     keys = {
-      { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
+      { "<C-b>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
       { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
     },
   config = function()
