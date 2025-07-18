4f8d44f0bff4eaaf022fbb6e91380c1fc6e09deb
Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>
2025-07-18 15:39:42 -0400

    Use pink for indent

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,7 +1,27 @@
 local highlight = {
-  "CursorColumn",
-  "Whitespace",
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
 }
+local hooks = require("ibl.hooks")
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#b35b79" })
+  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+vim.g.rainbow_delimiters = { highlight = highlight }
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
 
 return {
   "lukas-reineke/indent-blankline.nvim",
@@ -21,13 +41,9 @@ return {
       indent = {
         char = "│",
         tab_char = "│",
+        highlight = "LineNr",
       },
-      -- indent = { highlight = highlight, char = "" },
-      -- whitespace = {
-      --   highlight = highlight,
-      --   remove_blankline_trail = false,
-      -- },
-      scope = { show_start = true, show_end = true },
+      scope = { show_start = true, show_end = true, highlight = highlight },
       exclude = {
         filetypes = {
           "Trouble",
