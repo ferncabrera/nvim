b60aafe7f9b6b01eadb6445567d8ab6828ece57e
Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>
2025-08-13 20:44:46 -0400

    Updates;enhancements
    Signed-off-by: Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -2,7 +2,13 @@ return {
   "mbbill/undotree",
 
   config = function()
-    vim.keymap.set("n", "<leader>tu", vim.cmd.UndotreeToggle, { desc = "Better undotree!!!" })
+    vim.keymap.set("n", "<leader>tu", function()
+      local pickers = require("snacks.picker.core.picker").get()
+      if #pickers > 0 then
+        pickers[#pickers]:close()
+      end
+      vim.cmd.UndotreeToggle()
+    end, { desc = "Undotree" })
     -- configure settings via Vim globals
     vim.g.undotree_WindowLayout = 2 -- your layout config
     -- vim.g.undotree_SplitWidth = 40 -- your layout config
