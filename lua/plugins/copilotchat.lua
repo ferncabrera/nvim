0432c38198dcdcc1f3debcc462041d815216e0d1
Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>
2025-09-04 15:11:19 -0400

    Update

--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -9,7 +9,7 @@ return {
     return {
       -- model = "gpt-4.1",
       auto_insert_mode = true,
-      question_header = " " .. user .. " ",
+      -- question_header = " " .. user .. " ",
       headers = {
         user = " User (" .. user .. ") ", -- Header to use for user questions
         assistant = " Copilot ", -- Header to use for AI answers
