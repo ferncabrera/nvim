49f48bd82b6b98b85f6922e4aec5624ee4752dd7
Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>
2025-09-08 23:44:51 -0400

    feat(undotree): add picker close logic before toggling

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,10 +1,22 @@
+local function undotree_win_open()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    if vim.bo[buf].filetype == "undotree" then
+      return true
+    end
+  end
+  return false
+end
+
 return {
   "mbbill/undotree",
 
   config = function()
     vim.keymap.set("n", "<leader>tu", function()
+      local undotree_win = undotree_win_open()
       local pickers = require("snacks.picker.core.picker").get()
-      if #pickers > 0 then
+      -- Only close the picker if Undotree is NOT visible
+      if not undotree_win and #pickers > 0 then
         pickers[#pickers]:close()
       end
       vim.cmd.UndotreeToggle()
