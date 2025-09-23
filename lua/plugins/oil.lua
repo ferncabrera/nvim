7a7816d9bd9c2ee249fa3dc68305e8e6e8a98e6c
Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>
2025-09-22 20:09:22 -0400

    Oil working

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,33 +2,46 @@ return {
   {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
     keys = {
       { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
-      { "<space>-", function() require("oil").toggle_float() end, desc = "Open parent directory (float)" },
+      {
+        "<space>-",
+        function()
+          require("oil").toggle_float()
+        end,
+        desc = "Open parent directory (float)",
+      },
     },
     opts = {
+      default_file_explorer = false,
       columns = { "icon" },
       keymaps = {
         ["<C-h>"] = false,
+        ["q"] = { "actions.close", mode = "n" },
         ["<C-l>"] = false,
         ["<C-k>"] = false,
         ["<C-j>"] = false,
         ["<M-h>"] = "actions.select_split",
       },
-      win_options = {
-        winbar = "  %{v:lua.oil_get_winbar()}",
-      },
+      -- win_options = {
+      --   winbar = "%!v:lua.get_oil_winbar()",
+      -- },
       view_options = {
         show_hidden = true,
-        confirm = true, -- Enable confirmation popup for changes
       },
     },
     config = function(_, opts)
-      _G.oil_get_winbar = function()
-        local path = vim.fn.expand("%"):gsub("oil://", "")
-        return vim.fn.fnamemodify(path, ":.")
+      function _G.get_oil_winbar()
+        local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+        local dir = require("oil").get_current_dir(bufnr)
+        if dir then
+          return vim.fn.fnamemodify(dir, ":~")
+        else
+          -- If there is no current directory (e.g. over ssh), just show the buffer name
+          return vim.api.nvim_buf_get_name(0)
+        end
       end
-
       require("oil").setup(opts)
     end,
   },
