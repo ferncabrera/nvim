2aacfd2923df02fce51590839adcf12905f1dc1d
Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>
2025-09-24 00:17:07 -0400

    Updates

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
 -- Global variables.
 MAP = vim.keymap.set
 DEL = vim.keymap.del
+if vim.env.AUTO_NVIM_RESTORE == "1" then
+  vim.schedule(function()
+    require("persistence").load()
+  end)
+end
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
