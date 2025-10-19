a2bbbb6940961fbf317663f24deca8d3be8d569a
Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>
2025-10-19 13:53:35 -0400

    Dont restore sessions automatically

--- init.lua
+++ init.lua
@@ -17,10 +17,10 @@ end
 MODE = detect_dark_mode() and "dark" or "light"
 THEME = "wave"
 
-if vim.env.AUTO_NVIM_RESTORE == "1" then
-  vim.schedule(function()
-    require("persistence").load()
-  end)
-end
+-- if vim.env.AUTO_NVIM_RESTORE == "1" then
+--   vim.schedule(function()
+--     require("persistence").load()
+--   end)
+-- end
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
