e9115227b4cc24ee4b3caad48bb4ab9543deb9c7
Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>
2026-03-05 17:22:43 -0500

    udpates

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -249,6 +249,7 @@ return {
           breadcrumbs_section,
           vim.g.incline_show_diagnostics and props.focused and { get_diagnostic_label() } or {},
           git_diff_section,
+          { "", guifg = colors.bg, guibg = colors.fg },
           (not search_active) and { " ", guifg = colors.fg, guibg = colors.bg } or {},
           (not search_active) and { icon, guifg = colors.fg, guibg = colors.bg } or {},
           (not search_active) and { " ", guifg = colors.fg, guibg = colors.bg } or {},
